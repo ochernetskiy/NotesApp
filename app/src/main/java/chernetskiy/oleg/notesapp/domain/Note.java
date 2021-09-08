6c0623f7d5592fa0025b6068d6f36330d79d3612
Oleg <chernetskiyoleg09@gmail.com>
2021-09-08 23:46:22 +0300

    Third commit

--- app/src/main/java/chernetskiy/oleg/notesapp/domain/Note.java
+++ app/src/main/java/chernetskiy/oleg/notesapp/domain/Note.java
@@ -5,12 +5,15 @@ import androidx.annotation.StringRes;
 
 public class Note {
 
-    @StringRes
-    private int title;
-    @StringRes
-    private int description;
     @IntegerRes
     private final int date;
+
+    @StringRes
+    private int title;
+
+    @StringRes
+    private int description;
+
 
     public Note(int title, int description, int date) {
         this.title = title;
--- app/src/main/java/chernetskiy/oleg/notesapp/ui/NotesListFragment.java
+++ app/src/main/java/chernetskiy/oleg/notesapp/ui/NotesListFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +14,21 @@ import androidx.fragment.app.Fragment;
 import java.util.List;
 
 import chernetskiy.oleg.notesapp.R;
+import chernetskiy.oleg.notesapp.domain.DeviceNotesRepository;
 import chernetskiy.oleg.notesapp.domain.Note;
 
 public class NotesListFragment extends Fragment implements NotesListView {
+
+    private NotesListPresenter presenter;
+
+    private LinearLayout container;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        presenter = new NotesListPresenter(this, new DeviceNotesRepository());
+    }
 
     @Nullable
     @Override
@@ -27,13 +40,27 @@ public class NotesListFragment extends Fragment implements NotesListView {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        LinearLayout container = view.findViewById(R.id.root);
+        container = view.findViewById(R.id.root);
+
+        presenter.requestNotes();
 
 
     }
 
     @Override
     public void showNotes(List<Note> notes) {
+
+        for (Note note: notes) {
+
+            View noteItem = LayoutInflater.from(requireContext()).inflate(R.layout.item_note, container, false);
+
+            TextView noteTitle = noteItem.findViewById(R.id.note_title);
+
+            noteTitle.setText(note.getTitle());
+
+            container.addView(noteItem);
+
+        }
 
     }
 }
