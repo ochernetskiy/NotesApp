a642f4ac73482c4b9013f365ed48103975105d52
Oleg <chernetskiyoleg09@gmail.com>
2021-09-09 23:49:15 +0300

    Верхнее меню с кнопками "О приложении" и "Настройки"

--- app/src/main/java/chernetskiy/oleg/notesapp/ui/MainActivity.java
+++ app/src/main/java/chernetskiy/oleg/notesapp/ui/MainActivity.java
@@ -2,9 +2,12 @@ package chernetskiy.oleg.notesapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import chernetskiy.oleg.notesapp.R;
 import chernetskiy.oleg.notesapp.domain.Note;
@@ -21,6 +24,25 @@ public class MainActivity extends AppCompatActivity implements NotesListFragment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//
+//        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+//            @Override
+//            public boolean onMenuItemClick(MenuItem item) {
+//                if (item.getItemId() == R.id.about_app) {
+//                    Toast.makeText(MainActivity.this, "About App", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//
+//                if (item.getItemId() == R.id.settings) {
+//                    Toast.makeText(MainActivity.this, "Settings", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                }
+//
+//                return false;
+//            }
+//        });
 
     }
 
